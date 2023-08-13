9118e978147aed2447337424986b4da4ad22637e
Cugurel <cugurel7@gmail.com>
2023-08-13 13:26:00 +0300

    showButtomsheet implemented

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,11 +9,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-  List<Map<String, dynamic>> _allData=[];
+  List<Map<String, dynamic>> _allData = [];
   bool _isLoading = true;
 
-  void _refreshData() async{
+  void _refreshData() async {
     final data = await SQLHelper.getAllData();
     setState(() {
       _allData = data;
@@ -22,22 +21,116 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _refreshData();
   }
 
 
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _descController = TextEditingController();
 
-  Future<void> _addData() async{
+  Future<void> _addData() async {
     await SQLHelper.createData(_titleController.text, _descController.text);
     _refreshData();
   }
 
+  Future<void> _updateData(int id) async {
+    await SQLHelper.updateData(id, _titleController.text, _descController.text);
+    _refreshData();
+  }
+
+  void _deleteData(int id) async {
+    await SQLHelper.deleteData(id);
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        backgroundColor: Colors.redAccent, content: Text("Data deleted")));
+    _refreshData();
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descController = TextEditingController();
+
+  void showBottomSheet(int? id) async{
+    if(id!=null){
+      final existingData = _allData.firstWhere((element) => element['id']==id);
+      _titleController.text = existingData['title'];
+      _descController.text = existingData['desc'];
+    }
+
+    showModalBottomSheet(
+      elevation: 5,
+      isScrollControlled : true,
+      context:context,
+      builder:(_)=>Container(
+        padding: EdgeInsets.only(
+          top: 30,
+          left: 15,
+          right: 15,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 50,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Title"
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _descController,
+              maxLines: 4,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Description"
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFECEAF4),
+      appBar: AppBar(
+        title: Text("My Library"),
+      ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _allData.length,
+              itemBuilder: (context, index) => Card(
+                margin: EdgeInsets.all(15),
+                child: ListTile(
+                  title: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      _allData[index]['title'],
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  //subtitle: Text(),
+                ),
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+            showBottomSheet(null);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
